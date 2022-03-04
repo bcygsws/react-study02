a4ccf4b0dd4ee80f5ff1edb911662dc15dcc5170
BaoChengyi <bcygswspring168@gmail.com>
2022-03-04 23:55:58 +0800

    :memo: bind提交
    bind绑定this并提交参数的三种方式

--- src/components/ThisBind.jsx
+++ src/components/ThisBind.jsx
@@ -6,6 +6,15 @@
  */
 import React from 'react';
 export default class BindThis extends React.Component {
+	constructor(props) {
+		super(props);
+		// this指向实例
+		this.state = { msg: '' };
+		// a.第二种绑定方式：在构造函数中绑定
+		// b.bind绑定 后的返回值是原函数的一个拷贝，并包括改造后的this指向和参数
+		// 必须重新赋值，否则仅仅this.secondHandle.bind(this, '〽️', '⬅️');事件处理函数中的this还是指向undefined
+		this.secondHandle = this.secondHandle.bind(this, '〽️', '⬅️');
+	}
 	render() {
 		// console.log(this);// 渲染函数render中的this是当前组件的实例
 		return (
@@ -16,13 +25,19 @@ export default class BindThis extends React.Component {
 				{/* 插件Emoji Code的使用：键入ji根据提示选择需要的表情符号 */}
 				{/* 插件 Emoji插件，ctrl+shift+p 选择insert emoji ,进入选择emoji框，然后直接enter */}
 				{/* git Emoji commit中文版，点击源代码管理器中笑脸图标，会弹出选择emoji的下拉菜单供选择，即可切换 */}
+				<p>{this.state.msg}</p>
 				<input
 					type="button"
 					value="this的第一种传递参数的方式"
 					onClick={this.firstHandle.bind(this, '⭐', '⭕')}
 				/>
 				<hr />
-				<input type="button" value="第二种传参方式" />
+				{/* 在构造函数中为事件处理bind绑定this */}
+				<input
+					type="button"
+					value="第二种传参方式"
+					onClick={this.secondHandle}
+				/>
 			</div>
 		);
 	}
@@ -50,6 +65,35 @@ export default class BindThis extends React.Component {
 	 */
 	firstHandle(arg1, arg2) {
 		console.log(this); // 经过bind修改后，普通函数此处的this已经是当前组件实例了
-		console.log('传入的参数是：' + arg1 + '----' + arg2);
+		this.setState({
+			msg: 'bind的第一种绑定和传参方式：' + arg1 + arg2
+		});
+	}
+	secondHandle(arg1, arg2) {
+		console.log(this); // undefined
+		/**
+		 *
+		 * 在构造函数中bind了指向组件实例的this,然而此处仍然打印undefined
+		 * 解决方案：
+		 * 要接收以下绑定后的返回值
+		 * bind绑定是有返回值的，其返回值是：返回一个原函数的拷贝，并拥有指定的 this 值和初始参数
+		 * 见MDN文档：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+		 *
+		 *
+		 */
+		this.setState({
+			msg: '第二种传参方式：' + arg1 + '---------------' + arg2
+		});
 	}
 }
+
+/**
+ * 
+ * @ gitmoji-cli的使用
+ * 
+ * $ gitmoji -c 
+ * 然后，选择一种emoji,提示输入标题和message信息
+ * 接着，git push origin即可
+ * 
+ * 
+ */
