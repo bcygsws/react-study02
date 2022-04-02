227ecb8365846b7f6f715aca78ff93ed8ce8da1b
BaoChengyi <bcygswspring168@gmail.com>
2022-04-02 23:58:07 +0800

    :fire: 实现react中根目录的重定向

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 // 导入react-router相关的包，web中使用react-router-dom,可以根据是开发web还是App，选择安装不同的包
 // 按需导出常用的三个
-import { HashRouter, Route, Link, Switch } from 'react-router-dom';
+import { HashRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import loadable from './utils/loadable.js';
 // 导入views中的三个子组件
 import Home from './views/Home.jsx';
 // Home是主页一开始默认显示，Movie和About组件，使用react-loadable包动态导入
 const Movie = loadable(() => import('./views/Movie.jsx'));
 const About = loadable(() => import('./views/About.jsx'));
+// 导入route.less样式文件
+import RouteStyles from './css/route.less';
 // 引入DatePicker依赖的样式,在App.jsx文件中引入，安装并配置插件babel-plugin-import按需导入antd组件库
 // import 'antd/dist/antd.css';
 export default class App extends React.Component {
@@ -17,17 +19,26 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<HashRouter>
-				<div>
+				<div className={RouteStyles.container}>
 					<h3>这是App根组件</h3>
 					{/* 创建三个超链接 */}
-					<Link to="/home">首页</Link>
+					<NavLink to="/home" activeClassName={RouteStyles.selected}>
+						首页
+					</NavLink>
 					{/* 	<Link to="/movie">电影</Link> */}
 					{/* 1.默认路由规则中的匹配时模糊匹配的，Route 中/movie对应Movie中。若将Link中的路由改为to="/movie/top250/10"，
         仍可以匹配movie组件 */}
 					{/* 2.那么如何精确匹配呢？只需要在Route规则中响应位置添加属性exact即可，验证：/movie/top250/10 不能匹配到Movie组件了 */}
 					{/* 3.在Route规则中也配置参数，才能重新匹配Movie */}
-					<Link to="/movie/top250/10">电影</Link>
-					<Link to="/about">关于</Link>
+					<NavLink
+						to="/movie/top250/10"
+						activeClassName={RouteStyles.selected}
+					>
+						电影
+					</NavLink>
+					<NavLink to="/about" activeClassName={RouteStyles.selected}>
+						关于
+					</NavLink>
 					{/* 在同一时刻，只渲染一个组件，使用Switch标签包裹所有Route路由规则 */}
 					{/* v3 v4可以使用Route标签嵌套来实现路由嵌套，v5版本路由嵌套
 					参考文档：https://blog.csdn.net/Vue2018/article/details/100559895?utm_medium=distribute.pc_aggpage_search_result.none-task-blog-2~aggregatepage~first_rank_ecpm_v1~rank_v31_ecpm-2-100559895.pc_agg_new_rank&utm_term=hashrouter+%E5%B5%8C%E5%A5%97&spm=1000.2123.3001.4430 */}
@@ -40,6 +51,8 @@ export default class App extends React.Component {
 							exact
 						></Route>
 						<Route path="/about" component={About}></Route>
+						{/* 由根路径，重定向到/home配置Route */}
+						<Redirect from="/" to="/home" exact />
 					</Switch>
 				</div>
 			</HashRouter>
@@ -53,7 +66,12 @@ export default class App extends React.Component {
  * path="/"
  * component={组件名称}
  *
- * Link表示一个路由链接，类似Vue router-link
+ * 关于Link
+ * 参考文档：https://blog.csdn.net/lhjuejiang/article/details/80366839
+ * Link表示一个路由链接，类似Vue router-link。NavLink是Link的一个特定版，可以对路由进行修饰，
+ * a.有activeClassName和activeStyle来美化选中的那个路由
+ * b.isActive(function(){})链接是否激活的额外业务逻辑，例如：奇数时激活
+ *
  * 1.在src中创建一个根组件App，并导入到main.js中，与此同时App当做ReactDom.render(<App</App>)
  * 2.在App.jsx中按需导入,$ import {HashRouter,Route,Link} from 'react-router-dom';
  * 3.将App.jsx中的顶层div上加上一对HashRouter标签,HashRouter中只能出现一个根元素，上面组件中的div。HashRouter本身不会解析成
