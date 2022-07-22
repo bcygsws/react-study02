94b890ed4b9a934f75c8edd0f8b0ffa4e0c3cc47
bcygsws <bcygswspring168@gmail.com>
2022-07-23 03:04:50 +0800

    :art: 定义一个新的props保存props.color

--- src/components/new_circle/Fat.jsx
+++ src/components/new_circle/Fat.jsx
@@ -10,12 +10,14 @@
  */
 import React from 'react';
 import Son from './Son.jsx';
+import Son1 from './Son1.jsx';
 export default class Fat extends React.Component {
 	render() {
 		return (
 			<div>
 				<h4>父组件</h4>
 				<Son color="#ee2322"></Son>
+				<Son1 color="#aabbee"></Son1>
 			</div>
 		);
 	}
--- src/components/new_circle/Son1.jsx
+++ src/components/new_circle/Son1.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-export default class Son extends React.Component {
+export default class Son1 extends React.Component {
 	state = {
-		color: '#000000'
+		color: '#000000',
+		preColor: ''
 	};
 	render() {
 		return (
 			<div>
-				<h5>子组件Son</h5>
-				<p style={{ color: this.state.color }}>{this.state.color}</p>
+				<h5>子组件Son1</h5>
+				<p style={{ color: this.state.color }}>
+					{this.state.color}
+				</p>
 				<button onClick={this.handle}>
 					更新state中的color值，观察渲染文本的颜色
 				</button>
-				<hr />
 			</div>
 		);
 	}
@@ -24,6 +26,8 @@ export default class Son extends React.Component {
 	// 场景一、无条件从props更新state
 	/**
 	 * @ bug:
+	 * 参考文档：
+	 * 
 	 * 这里有一个bug，因为react@16.4版本以后，setState或者forceUpdate也会走getDerivedStateFromProps。直接
 	 * 从props得到state的更新值。父组件Fat传入的属性值不变；点击按钮时，即时使用了setState，color值也不会改变，
 	 * 一直是父组件传入的color值
@@ -34,10 +38,11 @@ export default class Son extends React.Component {
 	 *
 	 */
 	static getDerivedStateFromProps(props, state) {
-		if (props.color !== state.color) {
+		if (props.color !== state.preColor) {
 			// 无条件从props更新state
 			return {
-				color: props.color
+				color: props.color,
+				preColor: props.color
 			};
 		}
 		return null;
