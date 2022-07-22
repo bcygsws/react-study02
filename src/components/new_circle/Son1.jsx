ccf8079def56e73b8ab96dd373185d3bc1ede4a3
bcygsws <bcygswspring168@gmail.com>
2022-07-23 03:57:35 +0800

    :art: 受控组使用props更新state

--- src/components/new_circle/Son1.jsx
+++ src/components/new_circle/Son1.jsx
@@ -26,7 +26,7 @@ export default class Son1 extends React.Component {
 	// 场景一、无条件从props更新state
 	/**
 	 * @ bug:
-	 * 参考文档：
+	 * 参考文档：https://blog.csdn.net/weixin_43905830/article/details/108760828
 	 * 
 	 * 这里有一个bug，因为react@16.4版本以后，setState或者forceUpdate也会走getDerivedStateFromProps。直接
 	 * 从props得到state的更新值。父组件Fat传入的属性值不变；点击按钮时，即时使用了setState，color值也不会改变，
@@ -34,6 +34,8 @@ export default class Son1 extends React.Component {
 	 *
 	 * bug修复
 	 *
+	 * 当此时再点击按钮式，state中color和preColor属性值相等，不返回新的state状态
+	 * 此时，点击按钮，state状态时，返回null,即不更新状态；setState后就可以更新页面了
 	 *
 	 *
 	 */
