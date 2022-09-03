70488ff2993457ccf6cb97e1756c151b57120c32
bcygsws <bcygswspring168@gmail.com>
2022-09-04 03:57:35 +0800

    :art: 共同的父元素向兄弟元素传值

--- src/components/comunication/SiblingA.jsx
+++ src/components/comunication/SiblingA.jsx
@@ -8,7 +8,8 @@ export default class SiblingA extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			infoA: '这是SiblingA子组件的数据'
+			// 文本框的值
+			msg: ''
 		};
 	}
 	render() {
@@ -16,7 +17,13 @@ export default class SiblingA extends React.PureComponent {
 			<div>
 				<h3>这是SiblingA子组件</h3>
 				<p>{this.state.infoA}</p>
-				<input type="text" name="" id="txt" onChange={this.handleInput}/>
+				<input
+					type="text"
+					name=""
+					id="txt"
+					onChange={this.handleInput}
+					value={this.state.msg}
+				/>
 				<button onClick={this.handleA}>
 					向兄弟组件SiblingB传递数据
 				</button>
@@ -24,11 +31,17 @@ export default class SiblingA extends React.PureComponent {
 		);
 	}
 	// 处理文本框的输入
-	handleInput=()=>{
-
-	}
+	handleInput = (e) => {
+		// 为了拿到e.target数据，调用一下persist()方法；原因是：React 16中会将e.target数据对象重置，从而无法拿到e.target对象
+		e.persist();
+		// 测试
+		// console.log(e.target);
+		this.setState({
+			msg: e.target.value
+		});
+	};
 	handleA = () => {
 		// 点击按钮，向SiblingB组件传递数据
-		this.props.receiveA(this.state.infoA);
+		this.props.receiveA(this.state.msg);
 	};
 }
